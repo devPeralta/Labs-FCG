367de304fe7436f05715dbf8979822986a4ba2f6
Pablo Peralta <pdperalta@inf.ufrgs.br>
2025-04-26 14:52:32 -0300

    Lab 1 concluido, Lab2 concluido apenas matriz de transformacoes

---
+++ Laboratorio_1_Codigo_Fonte/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "/home/pabloperalta/Faculdade/fcg/Laboratorio_1_Codigo_Fonte/bin/Linux/main"
+  "/home/pabloperalta/Faculdade/fcg/Laboratorio_1_Codigo_Fonte/bin/Linux/main.pdb"
+  "CMakeFiles/main.dir/link.d"
+  "CMakeFiles/main.dir/src/glad.c.o"
+  "CMakeFiles/main.dir/src/glad.c.o.d"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
